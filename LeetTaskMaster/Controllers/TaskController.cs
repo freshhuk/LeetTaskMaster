b8daaeaa4b60f3ee003c64c875fb47ebb742f5a5
freshhuk <freshhuk13@gmail.com>
2023-08-18 22:28:59 +0300

    Added new file - controller

---
+++ LeetTaskMaster/Controllers/TaskController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeetTaskMaster.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        //метод для вывода всех имеющихся задачь
+        //[HttpGet]   
+
+    }
+}
