5923fa232d76c958a198af756c02bf907180468f
freshhuk <freshhuk13@gmail.com>
2023-08-18 22:09:28 +0300

    Добавьте файлы проекта.

---
+++ LeetTaskMaster/TaskServices/FibonacciNumbers.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetTaskMaster.TaskServices
+{
+    public class FibonacciNumbers
+    {
+        public bool IsFabonacci(int num)
+        {
+            int nextNum = 0;
+            int i = 1;
+            List<int> Fibonacci_numbers = new List<int>() { 0, 1};
+            while (nextNum <= num)
+            {
+                
+                nextNum = Fibonacci_numbers[i--] + Fibonacci_numbers[i];
+                Fibonacci_numbers.Add(nextNum);
+                i++;
+                if(nextNum == num) 
+                {
+                    //its fabonacci
+                    return true;
+                }
+               
+            }
+            return false;
+
+
+
+        }
+    }
+}
