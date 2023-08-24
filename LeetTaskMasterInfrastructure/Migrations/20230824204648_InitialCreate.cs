34362b357b3b4d4af7eea078388fd3b538615ec3
freshhuk <freshhuk13@gmail.com>
2023-08-24 23:49:38 +0300

    I FIX THIS, migration - done

---
+++ LeetTaskMasterInfrastructure/Migrations/20230824204648_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeetTaskMasterInfrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LeetTasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NameTask = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DescriptionTask = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LeetTasks", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LeetTasks");
+        }
+    }
+}
